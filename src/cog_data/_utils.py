2bddbb42bb43aaa1637fc3b0670f4ca7f41011cb
Ryan Soklaski <ry26099@mit.edu>
2022-06-28 00:14:14 -0400

    fix type annotation

--- src/cog_data/_utils.py
+++ src/cog_data/_utils.py
@@ -1,13 +1,16 @@
-from pathlib import Path
-from typing import Union
 import hashlib
-from hashlib import _Hash
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Union
 
+if TYPE_CHECKING:
+    from hashlib import _Hash
+else:
+    _Hash = Any
 
 __all__ = ["get_sha256_hash", "get_md5_hash"]
 
 
-def _get_hash(file_path: Union[Path, str], hash_fn: _Hash) -> str:   
+def _get_hash(file_path: Union[Path, str], hash_fn: _Hash) -> str:
     with open(file_path, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_fn.update(chunk)
@@ -22,4 +25,3 @@ def get_sha256_hash(file_path: Union[Path, str]) -> str:
 def get_md5_hash(file_path: Union[Path, str]) -> str:
     """Reads in data from disk and returns md5 hash"""
     return _get_hash(file_path, hashlib.md5())
-
