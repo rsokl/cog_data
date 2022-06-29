cd232038b2d0d9f5cb187a25815652bd0d3dcc68
Ryan Soklaski <ry26099@mit.edu>
2022-06-28 22:56:55 -0400

    add examples and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cog_data
 Hosts data for cogworks
+
+
+## For Maintainers
+
+To perform an editable install, clone this repo, navigate to its top level (to the directory that contains `src/` and `setup.py`) and run
+
+```console
+$ pip install -e .
+```
+
+Now any edits you make to the source code will immediately be reflected in your install.
+
--- src/cog_data/_utils.py
+++ src/cog_data/_utils.py
@@ -18,10 +18,24 @@ def _get_hash(file_path: Union[Path, str], hash_fn: _Hash) -> str:
 
 
 def get_sha256_hash(file_path: Union[Path, str]) -> str:
-    """Reads in data from disk and returns md5 hash"""
+    """Reads in data from disk and returns md5 hash
+    
+    Examples
+    --------
+    >>> from cog_data._utils import get_sha256_hash
+    >>> get_sha256_hash("./CogWeb/website_src/Language/Exercises/dat/stopwords.txt")
+    '9aa3e6f003d6c0e07a2d6e049e43a5669d554622dbd38fef91b37d4a852e15a5'
+    """
     return _get_hash(file_path, hashlib.sha256())
 
 
 def get_md5_hash(file_path: Union[Path, str]) -> str:
-    """Reads in data from disk and returns sha256 hash"""
+    """Reads in data from disk and returns sha256 hash
+    
+    Examples
+    --------
+    >>> from cog_data._utils import get_md5_hash
+    >>> get_md5_hash("./CogWeb/website_src/Language/Exercises/dat/stopwords.txt")
+    'a25f9890bfa50457244792b4b5c15566'
+    """
     return _get_hash(file_path, hashlib.md5())
